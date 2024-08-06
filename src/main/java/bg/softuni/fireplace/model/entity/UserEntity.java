262baccafb901078dcad30bb0da273f1977d19c0
George <george@gmail.com>
2024-08-06 21:35:06 +0300

    adding config

---
+++ src/main/java/bg/softuni/fireplace/model/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package bg.softuni.fireplace.model.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity{
+
+}
